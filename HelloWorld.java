9ff07ee2b1420bb58c641cd5e4a547bbe485cb0f
Jones, Ben <bmjones@mitre.org>
2017-08-08 14:36:35 -0400

    adding v1

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,3 +13,12 @@ public class HelloWorld {
     }
 }
 
+    private static String version = "3.3";
+    private static String featureList = "* point releases\n* master version";
+
+    public static void printRelese(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World.\nThis is version:"+version+"\nNow With Features:\n"+featureList);
+    }
+
+}
